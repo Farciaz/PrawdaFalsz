c6a53cbfee8195ad24d87672ee288e2dede86501
1796902 <1796902@TE-GDA-D-D21E23>
2024-10-21 13:24:53 +0200

    Dodaj pliki projektów.

---
+++ PrawdaFalsz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PrawdaFalsz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PrawdaFalsz/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace PrawdaFalsz
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           
+        }
+    }
+
+}
